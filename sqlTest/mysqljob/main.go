271827b422e68ae4bb592e7cd58783447fd7ce28
郭文帅 <guowenshuai8207@163.com>
2017-11-14 17:22:31 +0800

    go mysql

---
+++ sqlTest/mysqljob/main.go
@@ -0,0 +1,70 @@
+package main
+
+import(
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@/school?charset=utf8")
+	checkErr(err)
+
+	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
+	checkErr(err)
+
+	res, err := stmt.Exec("wenshuai", "pcb&&si", "2016-08-15")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println("insert id: ", id)
+
+	stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec("meimei", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println("affect:", affect)
+
+	rows, err := db.Query("select * from userinfo")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var departname string
+		var created string
+
+		err = rows.Scan(&uid, &username, &departname, &created)
+		checkErr(err)
+
+		fmt.Println(uid, username, departname, created)
+		
+	}
+
+	stmt, err = db.Prepare("delete from userinfo where uid=?")
+	checkErr(err)
+
+	res, err = stmt.Exec(id)
+	checkErr(err)
+	
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+	db.Close()
+	
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}	
+}
